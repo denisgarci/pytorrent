4b75536c0e9444dd129ebe643d67a2d0b3369fb6
Denis Garcia <denisgarci@gmail.com>
2013-10-22 18:11:57 -0400

    Added doctest

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 
 def bdecode(iostream):
     """
-    with io.open('simple_torrent.txt', 'rb') as iostream:
-        bdecode(iostream)
-    {'spam': ['a', 'b']}
+    #with io.open('simple_torrent.txt', 'rb') as iostream:
+        #bdecode(iostream)
+    #{'spam': ['a', 'b']}
     """
 
     def decode_str(c):
@@ -56,40 +56,31 @@ def bdecode(iostream):
     return decode_rules['d']('d')
 
 def bencode(data):
-    #"""
-    #>>> bencode({'spam': ['a', 'b']})
-    #d4:spaml1:a1:bee
-    #"""
+    """
+    >>> bencode(42)
+    'i42e'
+    >>> bencode('hello')
+    '5:hello'
+    >>> bencode(['a', 'b'])
+    'l1:a1:be'
+    >>> bencode({'spam': 1})
+    'd4:spami1ee'
+    >>> bencode({'spam': ['a', 'b']})
+    'd4:spaml1:a1:bee'
+    """
 
     def encode_int(integer):
-        """
-        >>> encode_int(42)
-        i42e
-        """
         return 'i{0}e'.format(integer)
 
     def encode_str(string):
-        """
-        >>> encode_str("")
-        "0:"
-        >>> encode_str("hello")
-        5:hello
-        """
         return '{0}:{1}'.format(len(string), string)
 
     def encode_list(a_list):
-        """
-        >>> encode_list(['a', 'b'])
-        l1:a1:be
-        """
-        return 'l{0}e'.format(''.join(encode(item) for item in a_list))
+        return 'l{0}e'.format(''.join(bencode(item) for item in a_list))
 
     def encode_dict(a_dict):
-        """
-        >>> encode_dict({'a':'b'})
-        d1:a1:be
-        """
-        return 'd{0}e'.format(''.join('{0}{1}'.format((bencode(key), bencode(value)) for key, value in a_dict.iteritems())))
+        #return 'd{0}e'.format(''.join('{0}{1}'.format(bencode(k), bencode(v)) for k,v in a_dict.items()))
+        return 'd{0}e'.format(''.join('{0}{1}'.format(bencode(key), bencode(value)) for key, value in a_dict.iteritems()))
 
     encode_rules = {int : encode_int,
                     str : encode_str,
